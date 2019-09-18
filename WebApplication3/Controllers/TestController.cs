fa0db601dff98e20d1d1a8e9efddf46838d61c89
Stephen Collins <scollins@asgresults.com>
2019-09-18 19:45:59 -0400

    Add project files.

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace WebApplication3.Controllers {
+
+    [ApiController]
+    public class TestController : ControllerBase {
+        [Authorize, HttpGet("claims")]
+        public IActionResult Claims() {
+            return Ok(User.Claims.ToDictionary(c => c.Type, c => c.Value));
+        }
+
+        [AllowAnonymous, HttpGet("token")]
+        public IActionResult Token() {
+            var handler = new JwtSecurityTokenHandler();
+            var key = Convert.FromBase64String("Jnl8XjBKeScpYzdHXWw/MytEQytNS3BLK25hOzNhWWM=");
+
+            var descriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim("group", "test")
+                    }, JwtBearerDefaults.AuthenticationScheme),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = handler.CreateToken(descriptor);
+            var jwt = handler.WriteToken(token);
+            return Ok(new { token = jwt });
+        }
+
+
+    }
+}
---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace WebApplication3 {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var key = Convert.FromBase64String("Jnl8XjBKeScpYzdHXWw/MytEQytNS3BLK25hOzNhWWM=");
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(o => {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = true;
+                    o.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
